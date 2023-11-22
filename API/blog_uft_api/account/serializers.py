70427361e9cc4b12a5175de6da01f0fd0bfd0643
GuiRosario <gdasilvarosario@gmail.com>
2023-11-22 19:43:09 -0300

    "add Blog Serializer"

--- API/blog_uft_api/account/serializers.py
+++ API/blog_uft_api/account/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.db.models import fields
+from .models import Blog
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +17,8 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ('titulo', 'texto', 'name', 'capa','categoria')
