d6e8325cb68f008a39e3a3a02d6686a6cd4d6a4b
GuiRosario <gdasilvarosario@gmail.com>
2023-11-30 23:37:25 -0300

    "add add get_blog view"

--- API/blog_uft_api/blogs/urls.py
+++ API/blog_uft_api/blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import add_blog
+from .views import add_blog, get_blog
 
 urlpatterns = [
-path('add_blog/', add_blog, name='add_blog'),
+    path('add_blog/', add_blog, name='add_blog'),
+    path('get_blog/<int:blog_id>', get_blog, name='blog_detail'),
 ]
--- API/blog_uft_api/blogs/views.py
+++ API/blog_uft_api/blogs/views.py
@@ -1,8 +1,9 @@
 from rest_framework import status
 from rest_framework.response import Response
-from .serializers import BlogSerializer
+from .serializers import BlogCreateSerializer, BlogSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from .models import Blog
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -11,10 +12,21 @@ def add_blog(request):
     dados = request.data.copy()
     dados.update({'usuario': request.user.id})
     print("userid",dados)
-    blog = BlogSerializer(data=dados)
+    blog = BlogCreateSerializer(data=dados)
     
     if blog.is_valid():
         blog.save()
         return Response(blog.data, status=status.HTTP_201_CREATED)
     else:
         return Response(blog.errors,status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_blog(request, blog_id):
+    try:
+        blog = Blog.objects.get(id=blog_id)
+    except Blog.DoesNotExist:
+        return Response({"error": "Blog not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = BlogSerializer(blog)
+    return Response(serializer.data, status=status.HTTP_200_OK)
